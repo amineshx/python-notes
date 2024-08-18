585349036aa0f5833351af9e393b04970de7a9e3
amineshx <amine.charef@etu.usthb.dz>
2024-08-19 00:11:11 +0100

    Update threading/03.py with args

--- threading/03.py
+++ threading/03.py
@@ -3,20 +3,20 @@ import time
 
 start=time.perf_counter()
 
-def do_somthing():
-    print("sleeping 1 sec")
-    time.sleep(1)
+def do_somthing(seconds):
+    print(f"sleeping {seconds} sec")
+    time.sleep(seconds)
     print("done sleeping")
 
 threads=[]
 for _ in range(10):
-    t=threading.Thread(target=do_somthing)
+    t=threading.Thread(target=do_somthing, args=[1.5])
     t.start()
     threads.append(t)
 
 for thread in threads:
     thread.join()
-    
+
 finish= time.perf_counter()
 
 print(f'Finished in {round(finish-start,2)} secs')
