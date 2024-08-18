24429b2318cee3a2e4b9d4052e01affabbee1abc
amineshx <amine.charef@etu.usthb.dz>
2024-08-18 23:53:14 +0100

    Update threadin/02.py adding join()

--- threading/02.py
+++ threading/02.py
@@ -12,6 +12,8 @@ t1=threading.Thread(target=do_somthing)
 t2=threading.Thread(target=do_somthing)
 t1.start()
 t2.start()
+t1.join()
+t2.join()
 finish= time.perf_counter()
 
 print(f'Finished in {round(finish-start,2)} secs')
