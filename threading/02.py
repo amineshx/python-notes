ef69ca510102d5a6f03e6b6b7698e8672e353a39
amineshx <amine.charef@etu.usthb.dz>
2024-08-18 23:52:26 +0100

    Update threadin/02.py

---
+++ threading/02.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+start=time.perf_counter()
+
+def do_somthing():
+    print("sleeping 1 sec")
+    time.sleep(1)
+    print("done sleeping")
+
+t1=threading.Thread(target=do_somthing)
+t2=threading.Thread(target=do_somthing)
+t1.start()
+t2.start()
+finish= time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} secs')
